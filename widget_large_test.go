5e442b47deedee38ef41d5d815cffef7f1fd6d07
RumbleFrog <xxlosttechxx@gmail.com>
2018-11-19 22:08:59 -0500

    widget: rework

---
+++ widget_large_test.go
@@ -0,0 +1,32 @@
+package dbl
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+var (
+	expectedLargeWidget      = BaseURL + "widgets/441751906428256277.png?certifiedcolor=343434&topcolor=fffff"
+	expectedLargeWidgetNoExt = BaseURL + "widgets/441751906428256277.svg?datacolor=309839"
+)
+
+func TestWidgetLarge(t *testing.T) {
+	w := NewLargeWidget().
+		BotID(testBotID).
+		Extension(PNG).
+		TopColor(0xfffff).
+		CertifiedColor(0x343434).
+		Generate()
+
+	assert.Equal(t, expectedLargeWidget, w)
+}
+
+func TestWidgetLargeNoExt(t *testing.T) {
+	w := NewLargeWidget().
+		BotID(testBotID).
+		DataColor(0x309839).
+		Generate()
+
+	assert.Equal(t, expectedLargeWidgetNoExt, w)
+}
---
+++ widget_small_test.go
@@ -0,0 +1,33 @@
+package dbl
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+var (
+	expectedSmallWidget       = BaseURL + "widgets/servers/441751906428256277.svg?leftcolor=555798&righttextcolor=f12350"
+	expectedSmallWidgetNoType = BaseURL + "widgets/status/441751906428256277.svg?avatarbg=987230&lefttextcolor=123890"
+)
+
+func TestWidgetSmall(t *testing.T) {
+	w := NewSmallWidget().
+		BotID(testBotID).
+		WidgetType(WidgetServers).
+		LeftColor(0x555798).
+		RightTextColor(0xf12350).
+		Generate()
+
+	assert.Equal(t, expectedSmallWidget, w)
+}
+
+func TestWidgetSmallNoType(t *testing.T) {
+	w := NewSmallWidget().
+		BotID(testBotID).
+		AvatarBackground(0x987230).
+		LeftTextColor(0x123890).
+		Generate()
+
+	assert.Equal(t, expectedSmallWidgetNoType, w)
+}
